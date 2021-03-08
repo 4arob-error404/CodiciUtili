3bc0b83e9cac2aed5af760571b26cdbce3d7bb1b
Michele Alladio <61046970+MicheleAlladioAKAMich@users.noreply.github.com>
2021-03-08 13:05:21 +0100

    Add files via upload
    Codice contenente tutte le costanti utilizzate per la creazione del videogioco

---
+++ config.py
@@ -0,0 +1,25 @@
+'''
+Author: Michele Alladio, Samuele Forneris, Alessandro Seimandi, Nicolò La Valle
+Descrizione:
+costanti utilizzate all'interno del videogioco
+'''
+
+#costanti generiche (adattate per la presona che ha creato il programma)  
+ALTEZZA = 700   #altezza schermata
+BASE = 1400 #base schermata
+Y_PREDEFINITA = 450 #y della macchina nella schermata PyGame
+SPAWN_1 = 80    #altezza che, una volta raggiunta dalla prima auto, fa spawnare la seconda macchina della fila
+SPAWN_2 = 100   #altezza che, una volta raggiunta dalla prima auto, fa spawnare la terza macchina della fila
+SPAWN_3 = 900   #altezza che, una volta raggiunta dalla prima auto, fa spawnare la prima macchina della fila
+X_SPAWN_CORSIA_1 = 450  #x della corsia 1
+X_SPAWN_CORSIA_2 = 650  #x della corsia 2
+X_SPAWN_CORSIA_3 = 860  #x della corsia 3
+ENEMY_WIDTH = 82    #base macchina nemico
+ENEMY_HEIGHT = 140  #altezza macchina nemico
+#limiti prestabiliti per lo spawn della seconda riga di nemici
+LIMITE1 = 300
+LIMITE2 = 100
+
+#costanti
+SCORE = 0   #punteggio
+SPEED = 10  #velocità
